a6993f488a62775a665c94c9bd3120a3aa5d21b7
Andrey Ozornin <andrey@ozorn.in>
2024-01-20 18:37:42 +0100

    live performance template

---
+++ performance.js
@@ -0,0 +1,22 @@
+// const canvasEl = document.getElementById('canvas');
+// const widthEl = document.getElementById('width');
+// const heightEl = document.getElementById('height');
+// const ruleEl = document.getElementById('rule');
+// const edgesEl = document.getElementById('edges');
+// const pixelsizeEl = document.getElementById('pixelsize');
+// const colorEl = document.getElementById('color');
+// const backgroundEl = document.getElementById('background');
+// const generateEl = document.getElementById('generate');
+// const densityEl = document.getElementById('density');
+
+// const form = document.getElementById('form');
+
+document.addEventListener('keydown', (e) => {
+    console.log({
+        key: e.key,
+        code: e.code,
+        keyCode: e.keyCode,
+        shift: e.shiftKey,
+        ctrl: e.ctrlKey,
+    })
+})
