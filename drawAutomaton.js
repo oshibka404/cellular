5048caa157b0f60474488579ff3467f179d6243d
Andrey Ozornin <andrey@ozorn.in>
2024-01-20 17:45:25 +0100

    performance optimisations and code separation

---
+++ drawAutomaton.js
@@ -0,0 +1,62 @@
+const _getEdgeCellState = (edges, isLeft, state) => {
+    if (edges === 'dead') {
+        return 0;
+    } else if (edges === 'alive') {
+        return 1;
+    } else if (edges === 'density') {
+        const density = parseFloat(densityEl.value)
+        return Math.random() < density ? 1 : 0;
+    } else if (edges === 'random') {
+        return Math.round(Math.random())
+    } else if (edges === 'loop') {
+        return isLeft ? state[state.length - 1] : state[0]
+    } else {
+        return Math.round(Math.random())
+    }
+}
+
+function _drawGeneration(state, i, ctx, pixelSize, color) {
+    ctx.fillStyle = color;
+    const y = i * pixelSize
+    for (let j = 0; j < state.length; j++) {
+        if (state[j]) {
+            ctx.fillRect(j * pixelSize, y, pixelSize, pixelSize);
+        }
+    }
+}
+
+function _updateState(prevState, ruleString, edges) {
+    const nextState = [];
+    for (let j = 0; j < prevState.length; j++) {
+        const left = j === 0 ? _getEdgeCellState(edges, true, prevState) : prevState[j - 1] || 0;
+        const center = prevState[j];
+        const right = j === prevState.length - 1 ? _getEdgeCellState(edges, false, prevState) : prevState[j + 1] || 0;
+
+        const rule = (left << 2) + (center << 1) + right; // 0-7
+        const newStateBit = parseInt(ruleString[7-rule], 10);
+        nextState.push(newStateBit);
+    }
+    return nextState;
+}
+
+function drawAutomaton({
+    ctx, 
+    width,
+    height,
+    pixelSize,
+    mainColor,
+    backgroundColor,
+    initialState,
+    ruleString,
+    edges,
+}) {
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(0, 0, width, height)
+
+    let state = initialState
+
+    for (let i = 0; i < height / pixelSize; i++) {
+        _drawGeneration(state, i, ctx, pixelSize, mainColor);
+        state = _updateState(state, ruleString, edges);
+    }
+}
--- script.js
+++ script.js
@@ -23,31 +23,6 @@ const getInitialState = (size) => {
     return state;
 }
 
-function drawGeneration(state, i, ctx, pixelSize, color, bgColor) {
-    for (let j = 0; j < state.length; j++) {
-        if (state[j]) {
-            ctx.fillStyle = color;
-        } else {
-            ctx.fillStyle = bgColor;
-        }
-        ctx.fillRect(j * pixelSize, i * pixelSize, pixelSize, pixelSize);
-    }
-}
-
-function updateState(prevState, ruleString) {
-    const nextState = [];
-    for (let j = 0; j < prevState.length; j++) {
-        const left = j === 0 ? getEdgeCellState(true, prevState) : prevState[j - 1] || 0;
-        const right = j === prevState.length - 1 ? getEdgeCellState(false, prevState) : prevState[j + 1] || 0;
-        const center = prevState[j];
-
-        const rule = (left << 2) + (center << 1) + right; // 0-7
-        const newStateBit = parseInt(ruleString[7-rule], 10);
-        nextState.push(newStateBit);
-    }
-    return nextState;
-}
-
 function generate() {
     const timer = performance.now()
     const width = parseInt(widthEl.value)
@@ -59,32 +34,19 @@ function generate() {
 
     const ruleString = parseInt(ruleEl.value, 10).toString(2).padStart(8, '0');
 
-    let state = getInitialState(Math.ceil(width / pixelSize));
-
-    for (let i = 0; i < height / pixelSize; i++) {
-        drawGeneration(state, i, ctx, pixelSize, colorEl.value, backgroundEl.value);
-        state = updateState(state, ruleString);
-    }
+    drawAutomaton({
+        ctx, 
+        width,
+        height,
+        pixelSize,
+        mainColor: colorEl.value,
+        backgroundColor: backgroundEl.value,
+        initialState: getInitialState(Math.ceil(width / pixelSize)),
+        ruleString,
+        edges: edgesEl.value,
+    });
 
     document.getElementById('running-time').innerHTML = `Generated in ${performance.now() - timer}ms`
-}
-
-const getEdgeCellState = (isLeft, state) => {
-    const edges = edgesEl.value
-    if (edges === 'dead') {
-        return 0;
-    } else if (edges === 'alive') {
-        return 1;
-    } else if (edges === 'density') {
-        const density = parseFloat(densityEl.value)
-        return Math.random() < density ? 1 : 0;
-    } else if (edges === 'random') {
-        return Math.round(Math.random())
-    } else if (edges === 'loop') {
-        return isLeft ? state[state.length - 1] : state[0]
-    } else {
-        return Math.round(Math.random())
-    }
 }
 
 form.addEventListener('change' , generate)
